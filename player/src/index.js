2ea6bf83ca53027db15df4a8207dcf306bd98f25
Alex Wilmer <djazium@gmail.com>
2019-09-11 16:44:35 -0400

    cleanup

--- player/src/index.js
+++ player/src/index.js
@@ -1,2 +1,26 @@
-import "./index.css";
-import "./main";
+import WebAudioScheduler from "web-audio-scheduler";
+import loadSamples from "utils/loadSamples";
+import play from "./play";
+
+let ctx = new AudioContext();
+let sched = new WebAudioScheduler({ context: ctx });
+
+let main = async () => {
+  let { _8BarBeats } = await loadSamples();
+
+  let currentBeat = 0;
+  let incrementBeat = () => currentBeat++;
+  let getCurrentBeat = () => currentBeat;
+
+  document.body.onclick = () => {
+    play({
+      ctx,
+      sched,
+      handleTimelineProgress: incrementBeat,
+      getCurrentBeat,
+      _8BarBeats
+    });
+  };
+};
+
+main();
